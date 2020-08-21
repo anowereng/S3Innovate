200f6e3197d9d120168047dac19452ed620926a0
anowereng <anowereng@gmail.com>
2020-08-21 14:45:01 +0600

    solution name change

--- Problem_1/Problem_1/Program.cs
+++ Problem_1/Problem_1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Problem_1
 {
@@ -6,7 +7,39 @@ namespace Problem_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Start time:");
+            string dateString = Console.ReadLine();  //"2019-08-31 7:10:24 am";
+            DateTime dateFromStringstart =
+                DateTime.Parse(dateString, System.Globalization.CultureInfo.InvariantCulture);
+
+
+            Console.Write("End time:");
+            string dateString2 = Console.ReadLine();  //"2019-08-31 7:10:24 am";
+            DateTime dateFromStringEnd =
+                DateTime.Parse(dateString2, System.Globalization.CultureInfo.InvariantCulture);
+
+
+            DateTime start = dateFromStringstart;
+            DateTime end = dateFromStringEnd;
+
+            double totalpoisa = 0;
+            var peakhourstart = new DateTime(dateFromStringstart.Year, dateFromStringstart.Month, dateFromStringstart.Day, 9, 0, 0);
+            var peakhourend = new DateTime(dateFromStringstart.Year, dateFromStringstart.Month, dateFromStringstart.Day, 22, 59, 59);
+            var offpeakhourstart1 = new DateTime(dateFromStringstart.Year, dateFromStringstart.Month, dateFromStringstart.Day, 23, 0, 0);
+            var offpeakhourend2 = new DateTime(dateFromStringstart.Year, dateFromStringstart.Month, dateFromStringstart.Day, 23, 59, 59);
+            var offpeakhourstart3 = new DateTime(dateFromStringstart.Year, dateFromStringstart.Month, dateFromStringstart.Day, 0, 0, 59);
+            var offpeakhourend4 = new DateTime(dateFromStringstart.Year, dateFromStringstart.Month, dateFromStringstart.Day, 8, 59, 59);
+            while (end > start)
+            {
+                start = start.AddSeconds(20);
+                if (start >= peakhourstart && start <= peakhourend) totalpoisa += 30;
+                if (start >= offpeakhourstart1 && start <= offpeakhourend2) totalpoisa += 20;
+                if (start >= offpeakhourstart3 && start <= offpeakhourend4) totalpoisa += 20;
+            }
+
+            Console.WriteLine(Convert.ToDouble(totalpoisa / 100) + "taka");
+
+            Console.ReadKey();
         }
     }
 }
