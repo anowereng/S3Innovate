d9f6c32c54cc24475a23910f91f42a6a25f718e7
anowereng <anowereng@gmail.com>
2020-08-22 11:46:15 +0600

    seed bug fixed

--- TimeSeriesChart.API/TimeSeriesChart.API/Data/Seed.cs
+++ TimeSeriesChart.API/TimeSeriesChart.API/Data/Seed.cs
@@ -15,19 +15,17 @@ namespace TimeSeriesChart.API
         {
             var context = new TimeseriesContext();
 
-            var result = context.Database.EnsureCreated();
+            context.Database.EnsureCreated();
 
-            if (!result)
+            if (!context.Buildings.Any())
             {
                 /*Building  Data*/
                 var buildings_data = await SeedHelper.SeedData<Building>("seed.buildings.json");
                 _ = context.AddRangeAsync(buildings_data);
-                //context.SaveChanges();
 
                 /*Datafield  Data*/
                 var datafield_data = await SeedHelper.SeedData<DataField>("seed.datafield.json");
                 _ = context.AddRangeAsync(datafield_data);
-                //context.SaveChanges();
 
                 /*object item  Data*/
                 var object_item_data = await SeedHelper.SeedData<ObjectItem>("seed.object-items.json");
